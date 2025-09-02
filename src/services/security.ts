d688ce856acec9d65afdbe7a498df469d48ddadc
Noman <pxycknomdictator@outlook.com>
2025-09-02 18:28:45 +0500

    feat: add services for creating tokens

--- src/services/security.ts
+++ src/services/security.ts
@@ -1,6 +1,8 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 import { logger } from "$lib/winston";
 import * as env from "$env/static/private";
+import type { JwtPayload, SignOptions } from "jsonwebtoken";
 
 export class PasswordService {
 	private readonly secret = Buffer.from(env.ARGON2_SECRET_KEY, "utf-8");
@@ -28,5 +30,51 @@ export class PasswordService {
 			logger.error("Failed to verify password");
 			throw error;
 		}
+	}
+}
+
+interface TokenPayload {
+	_id: string;
+	name: string;
+	email: string;
+}
+
+interface TokenResponse {
+	accessToken: string | null;
+	refreshToken: string | null;
+}
+
+export class TokenService {
+	private readonly accessTokenOptions: SignOptions = { expiresIn: "1d", algorithm: "HS256" };
+	private readonly refreshTokenOptions: SignOptions = { expiresIn: "7d", algorithm: "HS256" };
+
+	constructor(private readonly payload: TokenPayload) {}
+
+	public getTokens(): TokenResponse {
+		const accessToken = this.generateAccessToken(this.payload._id);
+		const refreshToken = this.generateRefreshToken(this.payload);
+
+		if (!accessToken || !refreshToken) {
+			logger.warn("Failed to generate tokens");
+			return { accessToken: null, refreshToken: null };
+		}
+
+		return { accessToken, refreshToken };
+	}
+
+	private generateAccessToken(_id: string): string {
+		return jwt.sign({ _id }, env.JWT_ACCESS_TOKEN_SECRET, this.accessTokenOptions);
+	}
+
+	private generateRefreshToken(payload: TokenPayload): string {
+		return jwt.sign(payload, env.JWT_REFRESH_TOKEN_SECRET, this.refreshTokenOptions);
+	}
+
+	public verifyAccessToken(token: string): JwtPayload {
+		return jwt.verify(token, env.JWT_ACCESS_TOKEN_SECRET) as JwtPayload;
+	}
+
+	public verifyRefreshToken(token: string): JwtPayload {
+		return jwt.verify(token, env.JWT_REFRESH_TOKEN_SECRET) as JwtPayload;
 	}
 }

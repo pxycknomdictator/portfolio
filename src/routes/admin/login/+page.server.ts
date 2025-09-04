116ed8b367b5732695f20465e828ad7f8c0d4668
Noman <pxycknomdictator@outlook.com>
2025-09-04 15:58:07 +0500

    feat: add cookies if admin is authentic

--- src/routes/admin/login/+page.server.ts
+++ src/routes/admin/login/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Actions } from "./$types";
 import { database } from "$lib/mongoose";
+import * as env from "$env/static/private";
 import { zodAdminSchema } from "$utils/zod";
 import { AuthService } from "$src/services/auth";
 import { zod4 } from "sveltekit-superforms/adapters";
@@ -12,7 +13,7 @@ export const load = async () => {
 };
 
 export const actions: Actions = {
-	async login({ request }) {
+	async login({ request, cookies }) {
 		await database();
 
 		const form = await superValidate(
@@ -32,6 +33,14 @@ export const actions: Actions = {
 		if (!response.success) {
 			return message(form, response.message, { status: 404 });
 		}
+
+		cookies.set("accessToken", response.accessToken, {
+			path: "/",
+			httpOnly: true,
+			maxAge: 60 * 60 * 24,
+			secure: env.NODE_ENV === "production",
+			sameSite: env.NODE_ENV === "production" ? "strict" : "lax"
+		});
 
 		return message(form, "Form Submitted Successfully");
 	}

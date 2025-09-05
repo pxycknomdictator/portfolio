24b2b351584aad3e1654efb37d3ceb9b40bb41b9
Noman <pxycknomdictator@outlook.com>
2025-09-05 13:02:55 +0500

    feat: add protected routes in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,15 @@
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 import { database } from "$lib/mongoose";
+
+const protectedRoutes = ["/admin/dashboard"];
 
 export const handle: Handle = async ({ event, resolve }) => {
 	await database();
+
+	if (protectedRoutes.includes(event.url.pathname)) {
+		const token = event.cookies.get("accessToken");
+		if (!token) throw redirect(303, "/admin/login");
+	}
+
 	return await resolve(event);
 };

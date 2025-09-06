25572346b72faefa044109b42389f04a5fae1fae
Noman <pxycknomdictator@outlook.com>
2025-09-06 20:29:40 +0500

    feat: add otp button for getting email

--- src/routes/admin/recovery/+page.svelte
+++ src/routes/admin/recovery/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	let disable = $state(false);
+
+	function disableBtn() {
+		disable = true;
+	}
+</script>
+
 <main class="grid h-screen w-screen place-items-center">
 	<form class="w-full max-w-2xl space-y-4 p-7">
 		<h1 class="mb-8 text-center text-xl sm:text-3xl">Recover your Password</h1>
@@ -28,11 +36,23 @@
 				/>
 			</div>
 		</section>
-		<div>
+		<div class="grid grid-cols-2 gap-x-2">
 			<button
-				class="w-full cursor-pointer rounded-sm bg-blue-500 py-2 font-semibold text-white transition-colors hover:bg-blue-600"
-				type="submit">Submit</button
+				disabled={disable}
+				type="button"
+				onclick={disableBtn}
+				class="w-full cursor-pointer rounded-sm border border-blue-500 py-2 font-semibold text-blue-500 transition-colors hover:bg-blue-100 disabled:cursor-not-allowed disabled:opacity-50"
 			>
+				Get Code
+			</button>
+			<button
+				disabled={disable}
+				class="w-full cursor-pointer rounded-sm bg-blue-500 py-2 font-semibold text-white transition-colors hover:bg-blue-600 disabled:cursor-not-allowed disabled:opacity-50"
+				type="submit"
+			>
+				Submit
+			</button>
+
 			<span class="mt-2.5 block">
 				Know the password?
 				<a class="text-blue-500 hover:underline" href="/admin/login">Login</a>
